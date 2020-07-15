696531730652d7101f0fef5bee52e33590488c9a
Lucas Campaner <lucas.campaner@grupoesfera.com.ar>
2020-07-15 17:15:36 -0300

    Agrega estructura inicial del proyecto.

---
+++ src/main/java/ar/com/inmobiliaria/spring/Program.java
@@ -0,0 +1,11 @@
+package ar.com.inmobiliaria.spring;
+
+public class Program {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Hola a todos!!!");
+    
+    }
+
+}
